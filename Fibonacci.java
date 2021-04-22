166d979459aae6752da6cf1ab893f10a231a3d99
iatudor <ionut.atudor@gmail.com>
2021-04-22 18:59:20 +0200

    Recursive Fibonacci

---
+++ Fibonacci.java
@@ -0,0 +1,18 @@
+public class Fibonacci {
+
+  public static void main(String[] args) {
+
+    int limite = Integer.parseInt(args[0]);
+
+    for (int i = 0; i < limite; i++) {
+      System.out.print(fibonacci(i) + " ");
+    }
+  }
+
+  private static int fibonacci(int num) {
+    if (num == 0 || num == 1)
+      return num;
+    else
+      return fibonacci(num - 1) + fibonacci(num - 2);
+  }
+}
